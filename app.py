c9e09b156316d5542292511f6af75516a6e848c1
abdurahman-ctis <abdurahman@ug.bilkent.edu.tr>
2018-10-02 17:44:32 +0300

    added urls.txt

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/index')
+# storing shortened urls
+urls = {}
+def loadurls():
+    with open("urls.txt") as f:
+        for line in f:
+            (key, val) = line.split()
+            urls[key] = val
+
+# hash function for urls
+def furyoku(url):
+    pass
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        url = request.form('url')
+        if urls.get(furyoku(url)):
+            print('exists')
+        else:
+            urls[furyoku(url)] = url
+        return render_template('index.html', url = urls.get(furyoku(url)))
 
 if __name__ == '__main__':
+    loadurls()
     app.run(debug=True)
