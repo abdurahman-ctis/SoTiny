7c1973b2618d5b33509da527695b14acbee1cf0a
abdurahman-ctis <abdurahman@ug.bilkent.edu.tr>
2018-10-02 23:26:11 +0300

    add url shortening with poor hashing

--- app.py
+++ app.py
@@ -1,18 +1,47 @@
 from flask import Flask, render_template, request
+from collections import OrderedDict
+import string
 
 app = Flask(__name__)
 
+#base url
+BASE_URL = 'http://localhost:5000/'
+
+# chars for hashing
+charset = [str(i) for i in range(10)]
+charset.extend(i for i in string.ascii_uppercase)
+charset.extend(i for i in string.ascii_lowercase)
+
 # storing shortened urls
-urls = {}
+urls = OrderedDict()
 def loadurls():
-    with open("urls.txt") as f:
-        for line in f:
-            (key, val) = line.split()
-            urls[key] = val
+    global urls
+    try:
+        with open("urls.txt") as f:
+            for line in f:
+                (key, val) = line.split()
+                urls[key] = val
+    except:
+        return "<h1>Error</h1>"
+
+# get next string for storing url recursively
+def increment(url, index):
+    if index<0:
+        return url
+    char = charset.index(url[index])+1
+    if char == len(charset):
+        url[index] = charset[0]
+        return increment(url, index-1)
+    else:
+        url[index] = charset[char]
+        return url
 
 # hash function for urls
 def furyoku(url):
-    pass
+    last = next(reversed(urls))
+    shamanlist = [i for i in last]
+    return ''.join(increment(shamanlist, len(shamanlist)-1))
+
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -20,12 +49,16 @@ def index():
     if request.method == 'GET':
         return render_template('index.html')
     else:
-        url = request.form('url')
-        if urls.get(furyoku(url)):
-            print('exists')
-        else:
-            urls[furyoku(url)] = url
-        return render_template('index.html', url = urls.get(furyoku(url)))
+        url = request.form['url']
+        short = furyoku(url)
+        if not url in urls.values():
+            print('NEW')
+            urls[short] = url
+            with open("urls.txt",'a') as f:
+                f.write(short+' '+url+'\n')
+        for k,v in urls.items():
+            if v == url:
+                return render_template('index.html', url = BASE_URL + k)
 
 if __name__ == '__main__':
     loadurls()
